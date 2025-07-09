3eb65ea7d4e259bf3021e10074ab01a6df1283dd
Purple <87976511+purplebarber@users.noreply.github.com>
2025-07-09 18:19:39 +0400

    Update __init__.py

--- sku/__init__.py
+++ sku/__init__.py
@@ -2,7 +2,7 @@ from sku.parser import Sku, get_schema
 from sku.models import itemClass
 from sku.schema import Schema
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 __all__ = ["Sku", "itemClass", "Schema", "get_schema", "update_schema"]
 
 # Main functions
